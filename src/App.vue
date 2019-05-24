d5e7869d803e2bacbde4c4451d492774b8f63aee
Yuko <yuko.mazurek@gmail.com>
2019-05-24 13:34:16 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <Header/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/Header";
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+};
+</script>
+
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
+</style>
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title">
+      <input type="submit" value="submit">
+    </form>
+  </div>
+</template>
+
+<script>
+// import uuid from "uuid";
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault(); // for form in html
+      const newTodo = {
+        // id: uuid.v4(),
+        title: this.title,
+        completed: false
+      };
+      //send up to parent
+      this.$emit("add-todo", newTodo);
+    }
+  }
+};
+</script>
+
+<style  scoped>
+</style>
